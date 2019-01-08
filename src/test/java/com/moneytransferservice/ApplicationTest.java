35db17f2341bba7be0bd6c5b0da1223594aadc6a
daniilperestoronin <perestoronin.daniil@gmail.com>
2019-01-08 19:19:49 +0300

    Update ApplicationTest

--- src/test/java/com/moneytransferservice/ApplicationTest.java
+++ src/test/java/com/moneytransferservice/ApplicationTest.java
@@ -11,106 +11,154 @@ import io.vertx.reactivex.ext.web.client.WebClient;
 import io.vertx.reactivex.ext.web.codec.BodyCodec;
 import org.javamoney.moneta.Money;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.zalando.jackson.datatype.money.MoneyModule;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DisplayName("Application Restfull Api test")
+@DisplayName("Application RESTfull Api test")
 @ExtendWith(VertxExtension.class)
 class ApplicationTest {
 
-    private final Account testAccount = new Account()
-            .setName("Test Account")
-            .setMoney(Money.of(1000, "USD"));
-    private final Account testAccount2 = new Account()
-            .setName("Test Account 2")
-            .setMoney(Money.of(1500, "USD"));
-    private final List accounts = List.of(testAccount, testAccount2);
+    private Account testAccount;
+    private Account testAccount2;
+    private Account testAccount3;
+    private Repository<Account> testAccountRepository;
+    private Repository<Transfer> testTransferRepository;
 
     @BeforeAll
     static void prepare() {
         Json.mapper.registerModule(new MoneyModule());
     }
 
-    @Test
-    @DisplayName("Account creation and reading test")
-    void testAccountApi(Vertx vertx, VertxTestContext testContext) {
-        WebClient webClient = WebClient.create(vertx);
-        vertx.deployVerticle(new Application(), testContext.succeeding(id -> {
-            webClient.get(8080, "localhost", "/account/")
-                    .as(BodyCodec.json(List.class))
-                    .send(testContext.succeeding(resp -> {
-                        testContext.verify(() -> {
-                            assertThat(resp.statusCode()).isEqualTo(200);
-                            assertThat(resp.body()).isEqualTo(Collections.emptyList());
-                            testContext.completeNow();
-                        });
-                    }));
-            webClient.post(8080, "localhost", "/account/")
-                    .as(BodyCodec.string())
-                    .sendJson(testAccount, testContext.succeeding(resp -> {
-                        testContext.verify(() -> {
-                            assertThat(resp.statusCode()).isEqualTo(201);
-                            testAccount.setId(UUID.fromString(resp.body()));
-                            testContext.completeNow();
-                        });
-                    }));
-            webClient.get(8080, "localhost", "/account/" + testAccount.getId())
-                    .as(BodyCodec.json(Account.class))
-                    .send(testContext.succeeding(resp -> {
-                        testContext.verify(() -> {
-                            assertThat(resp.statusCode()).isEqualTo(200);
-                            assertThat(resp.body()).isEqualTo(testAccount);
-                            testContext.completeNow();
-                        });
-                    }));
-            webClient.post(8080, "localhost", "/account/")
-                    .as(BodyCodec.string())
-                    .sendJson(testAccount2, testContext.succeeding(resp -> {
-                        testContext.verify(() -> {
-                            assertThat(resp.statusCode()).isEqualTo(201);
-                            testAccount2.setId(UUID.fromString(resp.body()));
-                            testContext.completeNow();
-                        });
-                    }));
-            webClient.get(8080, "localhost", "/account/" + testAccount2.getId())
-                    .as(BodyCodec.json(Account.class))
-                    .send(testContext.succeeding(resp -> {
-                        testContext.verify(() -> {
-                            assertThat(resp.statusCode()).isEqualTo(200);
-                            assertThat(resp.body()).isEqualTo(testAccount2);
-                            testContext.completeNow();
-                        });
-                    }));
-            webClient.get(8080, "localhost", "/account/")
-                    .as(BodyCodec.json(List.class))
-                    .send(testContext.succeeding(resp -> {
-                        testContext.verify(() -> {
-                            assertThat(resp.statusCode()).isEqualTo(200);
-                            assertThat(resp.body()).isEqualTo(accounts);
-                            testContext.completeNow();
-                        });
-                    }));
-        }));
+    @BeforeEach
+    void testDataPreparation() {
+        testAccount = new Account()
+                .setName("Test Account")
+                .setMoney(Money.of(1000, "USD"));
+        testAccount2 = new Account()
+                .setName("Test Account 2")
+                .setMoney(Money.of(1500, "USD"));
+        testAccount3 = new Account()
+                .setName("Test Account 3")
+                .setMoney(Money.of(2000, "USD"));
+        testAccountRepository = new Repository<>();
+        testTransferRepository = new Repository<>();
+        final var uuid = testAccountRepository.create(testAccount);
+        final var uuid2 = testAccountRepository.create(testAccount2);
+        final var uuid3 = testAccountRepository.create(testAccount3);
+        testAccount.setId(uuid);
+        testAccount2.setId(uuid2);
+        testAccount3.setId(uuid3);
     }
 
     @Test
-    @DisplayName("Money transfer test")
-    void testTransfer(Vertx vertx, VertxTestContext testContext) {
+    @DisplayName("readAll Accounts test")
+    void testReadAllAccounts(Vertx vertx, VertxTestContext testContext) {
+        WebClient webClient = WebClient.create(vertx);
+        vertx.deployVerticle(new Application(testAccountRepository, testTransferRepository),
+                testContext.succeeding(id ->
+                        webClient.get(8080, "localhost", "/account/")
+                                .as(BodyCodec.string())
+                                .send(testContext.succeeding(resp ->
+                                        testContext.verify(() -> {
+                                            assertThat(resp.statusCode()).isEqualTo(200);
+                                            assertThat(resp.body()).contains(
+                                                    Json.encode(testAccount),
+                                                    Json.encode(testAccount2),
+                                                    Json.encode(testAccount3)
+                                            );
+                                            testContext.completeNow();
+                                        })))));
+    }
 
-        final Repository<Account> accountRepository = new Repository<>();
-        final Repository<Transfer> transferRepository = new Repository<>();
-        final var uuid = accountRepository.create(testAccount);
-        final var uuid2 = accountRepository.create(testAccount2);
-        testAccount.setId(uuid);
-        testAccount2.setId(uuid2);
+    @Test
+    @DisplayName("read Account test")
+    void testReadAccount(Vertx vertx, VertxTestContext testContext) {
+        WebClient webClient = WebClient.create(vertx);
+        vertx.deployVerticle(new Application(testAccountRepository, testTransferRepository),
+                testContext.succeeding(id ->
+                        webClient.get(8080, "localhost",
+                                "/account/" + testAccount.getId().toString())
+                                .as(BodyCodec.json(Account.class))
+                                .send(testContext.succeeding(resp ->
+                                        testContext.verify(() -> {
+                                            assertThat(resp.statusCode()).isEqualTo(200);
+                                            assertThat(resp.body()).isEqualTo(testAccount);
+                                            testContext.completeNow();
+                                        })))));
+    }
+
+    @Test
+    @DisplayName("create Account test")
+    void testCreateAccount(Vertx vertx, VertxTestContext testContext) {
+        final var creationAccount = new Account()
+                .setName("Creation account")
+                .setMoney(Money.of(100, "USD"));
+        WebClient webClient = WebClient.create(vertx);
+        vertx.deployVerticle(new Application(testAccountRepository, testTransferRepository),
+                testContext.succeeding(id ->
+                        webClient.post(8080, "localhost", "/account/")
+                                .as(BodyCodec.string())
+                                .sendJson(creationAccount, testContext.succeeding(resp ->
+                                        testContext.verify(() -> {
+                                            assertThat(resp.statusCode()).isEqualTo(201);
+                                            final var creationAccountUuid = UUID.fromString(resp.body());
+                                            creationAccount.setId(creationAccountUuid);
+                                            assertThat(testAccountRepository.read(creationAccountUuid).get())
+                                                    .isEqualTo(creationAccount);
+                                            testContext.completeNow();
+                                        })))));
+    }
+
+    @Test
+    @DisplayName("update Account test")
+    void testUpdateAccount(Vertx vertx, VertxTestContext testContext) {
+        final var updatingAccount = new Account()
+                .setId(testAccount.getId())
+                .setName("Updating account")
+                .setMoney(Money.of(100, "USD"));
+        WebClient webClient = WebClient.create(vertx);
+        vertx.deployVerticle(new Application(testAccountRepository, testTransferRepository),
+                testContext.succeeding(id ->
+                        webClient.put(8080, "localhost", "/account/")
+                                .as(BodyCodec.string())
+                                .sendJson(updatingAccount, testContext.succeeding(resp ->
+                                        testContext.verify(() -> {
+                                            assertThat(resp.statusCode()).isEqualTo(200);
+                                            assertThat(testAccountRepository
+                                                    .read(updatingAccount.getId()).get())
+                                                    .isEqualTo(updatingAccount);
+                                            testContext.completeNow();
+                                        })))));
+    }
+
+    @Test
+    @DisplayName("delete Account test")
+    void testDeleteAccount(Vertx vertx, VertxTestContext testContext) {
+        WebClient webClient = WebClient.create(vertx);
+        vertx.deployVerticle(new Application(testAccountRepository, testTransferRepository),
+                testContext.succeeding(id ->
+                        webClient.delete(8080, "localhost", "/account/" + testAccount.getId().toString())
+                                .as(BodyCodec.json(Account.class))
+                                .send(testContext.succeeding(resp ->
+                                        testContext.verify(() -> {
+                                            assertThat(resp.statusCode()).isEqualTo(200);
+                                            assertThat(testAccountRepository
+                                                    .read(testAccount.getId()).isPresent())
+                                                    .isFalse();
+                                            testContext.completeNow();
+                                        })))));
+    }
+
+    @Test
+    @DisplayName("commit Money transfer test")
+    void testTransfer(Vertx vertx, VertxTestContext testContext) {
         final var amountMoney = Money.of(100, "USD");
         final var withdrawAccountMoney = testAccount.getMoney().subtract(amountMoney);
         final var addAccountMoney = testAccount2.getMoney().add(amountMoney);
@@ -120,18 +168,16 @@ class ApplicationTest {
                 .setAmount(amountMoney);
 
         WebClient webClient = WebClient.create(vertx);
-        vertx.deployVerticle(new Application(accountRepository, transferRepository)
-                , testContext.succeeding(id -> {
-                    webClient.post(8080, "localhost", "/transfer/commit")
-                            .as(BodyCodec.string())
-                            .sendJson(transfer, testContext.succeeding(trResp -> {
-                                testContext.verify(() -> {
-                                    assertThat(trResp.statusCode()).isEqualTo(200);
-                                    assertThat(testAccount.getMoney()).isEqualTo(withdrawAccountMoney);
-                                    assertThat(testAccount2.getMoney()).isEqualTo(addAccountMoney);
-                                    testContext.completeNow();
-                                });
-                            }));
-                }));
+        vertx.deployVerticle(new Application(testAccountRepository, testTransferRepository),
+                testContext.succeeding(id ->
+                        webClient.post(8080, "localhost", "/transfer/commit")
+                                .as(BodyCodec.string())
+                                .sendJson(transfer, testContext.succeeding(trResp ->
+                                        testContext.verify(() -> {
+                                            assertThat(trResp.statusCode()).isEqualTo(200);
+                                            assertThat(testAccount.getMoney()).isEqualTo(withdrawAccountMoney);
+                                            assertThat(testAccount2.getMoney()).isEqualTo(addAccountMoney);
+                                            testContext.completeNow();
+                                        })))));
     }
 }
